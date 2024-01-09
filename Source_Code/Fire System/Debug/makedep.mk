828cb2ec156d1d1f80f6fba1bb984f40e119be41
Mohamed Sayed Abd El-Rahman <128575094+msayed6789@users.noreply.github.com>
2024-01-09 03:44:30 +0200

    Add files via upload

---
+++ Source_Code/Fire System/Debug/makedep.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+HAL\KeyPad.c
+
+HAL\LCD.c
+
+HAL\Motor.c
+
+HAL\Sensors.c
+
+main.c
+
+MCAL\ADC.c
+
+MCAL\DIO.c
+
+MCAL\DIO_Lcfg.c
+
+MCAL\EX_Interrupt.c
+
